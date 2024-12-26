3521de13b988a391bb24346684de560e22f160b7
GustavoKasaki <gmkasaki@gmail.com>
2024-12-26 12:35:04 -0300

    Atualizada a requisição API para RTK Query

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,15 +1,17 @@
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
 
 type Props = {
-  produtos: ProdutoType[]
   favoritos: ProdutoType[]
   favoritar: (produto: ProdutoType) => void
 }
 
-const ProdutosComponent = ({ produtos, favoritos, favoritar }: Props) => {
+const ProdutosComponent = ({ favoritos, favoritar }: Props) => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
     const IdsDosFavoritos = favoritos.map((f) => f.id)
@@ -17,20 +19,23 @@ const ProdutosComponent = ({ produtos, favoritos, favoritar }: Props) => {
     return IdsDosFavoritos.includes(produtoId)
   }
 
-  return (
-    <>
-      <S.Produtos>
-        {produtos.map((produto) => (
-          <Produto
-            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
-            key={produto.id}
-            produto={produto}
-            favoritar={favoritar}
-          />
-        ))}
-      </S.Produtos>
-    </>
-  )
+  if (isLoading) return <h2>Carregando...</h2>
+  else {
+    return (
+      <>
+        <S.Produtos>
+          {produtos?.map((produto) => (
+            <Produto
+              key={produto.id}
+              produto={produto}
+              estaNosFavoritos={produtoEstaNosFavoritos(produto)}
+              favoritar={favoritar}
+            />
+          ))}
+        </S.Produtos>
+      </>
+    )
+  }
 }
 
 export default ProdutosComponent
